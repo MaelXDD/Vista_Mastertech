4e22403599b1fc357f7cba719da125c0c758d36e
MaelXD <fiestasjheins@gmail.com>
2026-06-02 21:27:28 -0500

    Estructura

--- phantom-frontend/src/app/app.routes.ts
+++ phantom-frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../app/core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+
+  // Auth: login y registro
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('../app/modules/modules/auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+
+  // Productos (público)
+  {
+    path: 'productos',
+    loadComponent: () =>
+      import('../app/modules/modules/productos/pages/lista-productos/lista-productos.component')
+        .then(m => m.ListaProductosComponent)
+  },
+
+  // Wildcard
+  { path: '**', redirectTo: 'productos' }
+];
--- phantom-frontend/src/components/home/home.ts
+++ phantom-frontend/src/components/home/home.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoService } from '../../services/producto';
+import { Producto } from '../../models/producto';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class HomeComponent implements OnInit {
+  // Arreglo donde se guardarán los productos traídos de la base de datos
+  listaProductos: Producto[] = [];
+  mensajeError: string = '';
+
+  // Inyección del servicio a través del constructor
+  constructor(private productoService: ProductoService) {}
+
+  ngOnInit(): void {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    this.productoService.listarTodos().subscribe({
+      next: (data) => {
+        this.listaProductos = data;
+        console.log('Datos recibidos correctamente del backend:', this.listaProductos);
+      },
+      error: (err) => {
+        this.mensajeError = 'No se pudo conectar con el servidor backend.';
+        console.error('Error de conexión:', err);
+      },
+    });
+  }
+}
